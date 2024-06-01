d9d6f9e58e4012383727542cee1cdaa1e1e588c2
Scryng <scrynng@gmail.com>
2024-06-01 06:02:55 -0300

    feat: Add Queue class

--- Group.lua
+++ Group.lua
@@ -12,10 +12,13 @@ function Group:new()
     group.amountPlayers = 0
     
     -- Methods
+
+    -- Get number of players
     function group:getAmountPlayers()
         return self.amountPlayers
     end
     
+    -- Add one more player to the number of current players in the group
     function group:setNewPlayerInAmount()
         if self.amountPlayers < self.maxPlayers then
             self.amountPlayers = self.amountPlayers + 1
@@ -24,23 +27,28 @@ function Group:new()
         end
     end
     
+    -- Remove a player from the number of current players in the group
     function group:removePlayerOfAmount()
         self.amountPlayers = self.amountPlayers - 1
     end
     
+    -- Get status of group is ready
     function group:getStatus()
         return self.status
     end
     
+    -- Set status of group is ready
     function group:setStatus(status)
         self.status = status
     end
     
+    -- Add a player in this group
     function group:addPlayer(player)
         table.insert(self.players, player)
         self:setNewPlayerInAmount()
     end
     
+    -- Get players in this group
     function group:getPlayers()
         return self.players
     end
